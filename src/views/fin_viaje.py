2f9457565b89df12e696be667377b40508c41d3d
Aldryn6 <aldrynrodriguezguzman@gmail.com>
2025-05-05 11:25:17 +0200

    Final_viaje

---
+++ src/views/fin_viaje.py
@@ -0,0 +1,57 @@
+import flet as ft
+
+play_store_url="https://play.google.com/store/apps/details?id=com.playstack.balatro.android"
+
+
+def get_fin_viaje_view(page: ft.Page) -> ft.View:
+    page.title = "TransportHelper Fin del viaje"
+
+    # Modal dialog definition
+    te_vas_pa_la_playstore_jimbo = ft.AlertDialog(
+        modal=True,
+        title=ft.Text("Valorar aplicación"),
+        content=ft.Text("Serás redirigido a la tienda de aplicaciones"),
+        actions=[
+            ft.TextButton("Ok", on_click=lambda e: (page.launch_url(play_store_url),page.close(te_vas_pa_la_playstore_jimbo))),
+            ft.TextButton("No", on_click=lambda e: (page.close(te_vas_pa_la_playstore_jimbo),page.go("/inicio"))),
+        ],
+        actions_alignment=ft.MainAxisAlignment.END,
+    )
+
+    boton_si_valora = ft.ElevatedButton(
+        "Si",
+        on_click=lambda e: page.open(te_vas_pa_la_playstore_jimbo)
+    )
+    boton_no_valora = ft.ElevatedButton("No",on_click=lambda e: page.go("/inicio"))
+
+    # Return view
+    return ft.View(
+        "/finviaje",
+        controls=[
+            ft.SafeArea(
+                ft.Container(
+                    ft.Column(
+                        [
+                            ft.Image(
+                                src="src/assets/bus_black.png" if page.theme_mode == ft.ThemeMode.DARK else "src/assets/bus_not_black.png",
+                                width=150,
+                                height=150,
+                                fit=ft.ImageFit.CONTAIN,
+                            ),
+                            ft.Image(src="src/assets/estrella_fin.svg",width=150,height=150,fit=ft.ImageFit.CONTAIN),
+                            ft.Divider(height=20, color="transparent"),
+                            ft.Text("Nos das tu opinión sobre el viaje?", size=30, weight=ft.FontWeight.BOLD),
+                            boton_si_valora,
+                            boton_no_valora,
+                        ],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                        spacing=20,
+                    ),
+                    padding=40,
+                    alignment=ft.alignment.center,
+                ),
+                expand=True,
+            )
+        ]
+    )
